683e4c185164c8154da9a1fb733492e2115690fa
Rexben <rexben.rb@gmail.com>
2021-02-07 00:49:28 +0100

    updates

--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,19 @@ const Axios = require('axios');
 
 router.get('/alert', async (req, res) => {
   try {
-    const val = 'Me';
-    const { resp } = await Axios.post(process.env.WEBHOOKS, {
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `Alert! Alert! *${val} is thanks. <https://www.benjaminajewole.com| View my website>`,
-          },
-        },
-      ],
-    });
-    console.log('resp>>>', resp);
+    // const val = 'Me';
+    // const { resp } = await Axios.post(process.env.WEBHOOKS, {
+    //   blocks: [
+    //     {
+    //       type: 'section',
+    //       text: {
+    //         type: 'mrkdwn',
+    //         text: `Alert! Alert! *${val} is thanks. <https://www.benjaminajewole.com| View my website>`,
+    //       },
+    //     },
+    //   ],
+    // });
+    // console.log('resp>>>', resp);
     res.json({ date: new Date().toISOString() });
   } catch (error) {
     console.error('err>>', error);
