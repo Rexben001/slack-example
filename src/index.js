eede08062313ba76d7ab0927d1c496a5493e2113
Rexben <rexben.rb@gmail.com>
2021-02-07 00:24:08 +0100

    update port

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ const routes = require('./routes');
 
 const app = express();
 
+const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use('/', routes);
 
-app.get('/', (req, res) => res.send('Start APp'));
+app.get('/', (req, res) => res.send('API is running!!!'));
 
-app.listen(4000, () => console.log('Listening to PORT 4000'));
+app.listen(PORT, () => console.log(`Listening to PORT ${PORT}`));
